51ceac3a68aff315a90d5575f8f0401944168091
Iann Lucas <contact.iannlucas@gmail.com>
2024-10-06 14:44:36 -0300

    Aprendi sobre loopings

---
+++ aula13/ambiente.js
@@ -0,0 +1,33 @@
+/*
+
+let fatias = 1
+do{
+    console.log(`A ${fatias}º é muito boa`)             MODO: DO
+    fatias++
+
+} while(fatias <=10)
+
+*/
+
+
+
+
+/*
+
+let fatias = 1
+while (fatias <= 10) {
+    console.log(`A ${fatias}º é muito boa`)             MODO: WHILE
+    fatias++
+}
+
+
+*/
+
+
+/*
+    let fatias = 1;
+for(let fatias = 1 ; fatias <= 10 ; fatias++) {
+    console.log(`A ${fatias}º de pizza é muito boa`)                MODO: FOR
+}
+
+*/
