3e933bcdc2a7fcacc578228bba02a9e4b1a3c396
Iann Lucas <contact.iannlucas@gmail.com>
2024-10-10 20:21:10 -0300

    Aprendi sobre array

---
+++ aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9, 3]
+console.log(`O vetor tem ${num.length} posições`)
+console.log(num)
+
+
+
+let pos = num.indexOf(10)
+
+if (pos == -1) {
+    console.log('Numero não achado')
+}
+console.log(pos)
