9f4150921712820d1223903f870c715bf113e7cb
Iann Lucas <contact.iannlucas@gmail.com>
2024-10-11 22:09:16 -0300

    Fiz mais ex para arrays

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,27 @@
+let carros = [" Ferrari F40LM , Skyline R34 , M4 COMPETITION , Fusca Bumblebee"]  // os valores
+carros.push(' R35') // forma de adicionar um valor na variavel
+console.log(`Os carros do estacionamento são ${carros}`)
+
+                                                                            // MINHA VERSÃO 
+for (let pos in carros) {  // método for in 
+    console.log(`A vaga ${pos} tem o ${carros[pos]}`)
+}   
+
+
+
+/*   Versao CHATGPT
+
+let carros = ["Ferrari F40LM", "Skyline R34", "M4 COMPETITION", "Fusca Bumblebee"];
+carros.push("R35");
+
+console.log(`Os carros do estacionamento são: ${carros.join(', ')}`);
+
+for (let pos in carros) {
+    console.log(`A vaga ${parseInt(pos) + 1} tem o carro: ${carros[pos]}`);
+}
+
+
+/*
 let num = [5, 8, 2, 9, 3]
 console.log(`O vetor tem ${num.length} posições`)
 console.log(num)
@@ -10,3 +34,5 @@ if (pos == -1) {
     console.log('Numero não achado')
 }
 console.log(pos)
+
+*/
