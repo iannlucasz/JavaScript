f14188f3fc29e3b111ceb0eabb15bce574e4b30c
Iann Lucas <contact.iannlucas@gmail.com>
2024-11-06 17:11:39 -0300

    Vi sobre loop com for e objetos

---
+++ B7Web/objetos/script.js
@@ -0,0 +1,17 @@
+const eu = {
+    nome: "Iann Lucas",
+    idade: "15 anos",
+    kilogramas: "81kg",
+}
+
+console.log(`Olá meu nome é ${eu.nome} , tenho ${eu.idade} e peso ${eu.kilogramas}`)
+
+
+const estacionamento = {
+    carros: [
+        {modelo: "Ford Fiesta", ano: "2008"},
+        {modelo: "Nissan Skyline R34", cor: "BlueSky"}
+    ]
+}
+
+console.log(estacionamento.carros[0].ano)
