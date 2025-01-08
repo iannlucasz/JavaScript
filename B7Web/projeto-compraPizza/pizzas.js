d879ab260cd8e1448eff66a847dfa167ecf98fab
Iann Lucas <contact.iannlucas@gmail.com>
2025-01-08 17:50:07 -0300

    Fiz Uma parte do Projeto Pizza

---
+++ B7Web/projeto-compraPizza/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Clássica e deliciosa, coberta com queijo mussarela derretido e orégano.'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Fatias de calabresa suculenta com cebola e aquele toque especial de orégano.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'A perfeita combinação de mussarela, provolone, parmesão e gorgonzola.'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Mussarela, presunto, milho, bacon e aquele sabor irresistível.'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Massa crocante coberta com sorvete cremoso e calda de chocolate.'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Uma criação especial com os melhores ingredientes da casa. Surpreenda-se!'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Massa doce com chocolate derretido, perfeita para os chocólatras de plantão.'}
+];
